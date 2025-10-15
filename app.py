60499dc8e7d29087b344fcd37dd31a0627c9c288
Alan Thomas <alanthomas860@gmail.com>
2025-10-15 22:49:52 +0530

    updated hello world message

--- app.py
+++ app.py
@@ -5,7 +5,11 @@ app = Flask(__name__)
 @app.route('/')
 
 def hello_world():
+<<<<<<< HEAD
     return 'Hello world from CI/CD'
+=======
+    return 'Hello world, this is CICD pipeline'
+>>>>>>> 4dc0503 (Updated the message)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
