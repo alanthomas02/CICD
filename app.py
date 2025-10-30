eb628821e1f5a1698cf900ce752bf33004c319c5
ALAN THOMAS <alanthomas860@gmail.com>
2025-10-30 19:50:24 +0530

    Fix typo in CI/CD pipeline message

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 
 def hello_world():
-    return 'Hello world, this is CICD pipeline'
+    return 'Hello world, this is CI/CD pipeline'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
